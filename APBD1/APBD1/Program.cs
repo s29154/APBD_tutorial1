e3e32c1fee41ddd913ff9721dad54229633e2197
s29154 <s29154@pjwstk.edu.pl>
2025-10-15 19:08:08 +0200

    modified var name

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -8,7 +8,7 @@ class Program {
 
     static double average(int[] arr)
     {
-        double sum = arr.Aggregate<int, double>(0, (current, t) => current + t);
+        double sum = arr.Aggregate<int, double>(0, (cur, t) => cur + t);
         return sum / arr.Length;
     }
 
