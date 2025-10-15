76072731cabd9d75e5bd6a4a5dae19d00679d4d1
s29154 <s29154@pjwstk.edu.pl>
2025-10-15 18:48:12 +0200

    added average method

--- APBD1/APBD1/Program.cs
+++ APBD1/APBD1/Program.cs
@@ -5,4 +5,14 @@ class Program {
     static void Main() {
         Console.WriteLine("Sup?");
     }
+
+    static double average(int[] arr)
+    {
+        double sum = 0;
+        for (int i =0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum / arr.Length;
+    }
 }
